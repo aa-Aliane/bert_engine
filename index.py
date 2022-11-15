33b947e6d2c58e96e95a0340b6d93bd421e97702
aaa29 <aliane781@gmail.com>
2022-11-15 10:00:04 +0100

    5c

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
-import faiss
+import os, faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('all-MiniLM-L12-v1')
+
+
+
+
+
+alldocs = []
+
+for name in os.listdir('extracted'):
+    with open('extracted/'+name, 'r', encoding="utf8") as f:
+        alldocs += [f.read()]
+        
+encoded_data = model.encode(alldocs)
+encoded_data = np.asarray(encoded_data.astype('float32'))
+index = faiss.IndexIDMap(faiss.IndexFlatIP(768))
+index.add_with_ids(encoded_data, np.array(range(0, len(alldocs))))
+faiss.write_index(index, 'asjp.index')
