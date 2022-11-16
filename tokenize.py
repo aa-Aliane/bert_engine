d5878373742a38e46c838937e98102a3aa4421d3
aaa29 <aliane781@gmail.com>
2022-11-16 10:09:22 +0100

    wc4

--- tokenize.py
+++ tokenize.py
@@ -14,16 +14,10 @@ for name in track(os.listdir('extracted'), description="Processing..."):
     with open('extracted/'+name, 'r', encoding="utf8") as f:
         text = f.read()
         output: str = segmenter.normalize_and_segment(text)
-        print(output)
-
-        # Output of segment is str.
-        # To get tokens, you can split by whitespace.
         tokens = output.split()
-        print(tokens)
-        # # Desegment
-        # output: str = segmenter.desegment(text)
+
         paragraphs = ' '.join(tokens).split('.')
-        print(paragraphs)
+
         
         for i, p in enumerate(paragraphs):
             if p:
