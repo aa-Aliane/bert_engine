b499f5aa0efed786084d9c232a8060af7ed58d39
aaa29 <aliane781@gmail.com>
2022-11-16 09:48:01 +0100

    wc1

---
+++ tokenize.py
@@ -0,0 +1,32 @@
+import fasttokenizer, os
+
+from tqdm import tqdm
+
+
+segmenter = fasttokenizer.Segmenter()
+
+
+
+
+
+
+
+for name in tqdm(os.listdir('extracted')):
+    with open('extracted/'+name, 'r', encoding="utf8") as f:
+        text = f.read()
+        output: str = segmenter.normalize_and_segment(text)
+        print(output)
+
+        # Output of segment is str.
+        # To get tokens, you can split by whitespace.
+        tokens = output.split()
+        print(tokens)
+        # # Desegment
+        # output: str = segmenter.desegment(text)
+        paragraphs = ' '.join(tokens).split('.')
+        print(paragraphs)
+        
+        for i, p in enumerate(paragraphs):
+            with open('paragraphs/'+name+'_'+str(i), 'w', encoding="utf8") as f:
+                f.write(p)
+    
